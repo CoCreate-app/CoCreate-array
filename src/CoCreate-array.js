2f5923da48067259191c89b5df3014c380bec840
jeanmendoza <janco1187@gmail.com>
2020-07-15 03:41:56 +0000

    de

--- src/CoCreate-array.js
+++ src/CoCreate-array.js
@@ -2,7 +2,7 @@ var arrayClass = 'arrayField';
 
 initSocketsForArray();
 initArrayForms();
-//-23
+//-2
 function initSocketsForArray() {
   CoCreateSocket.listen('connect', function (data, room) {
     console.log('socket connected');
